7a8ef6074be0953c37af78d0e07ac18afe76a2b5
Rohitkumar25t <139347131+Rohitkumar25t@users.noreply.github.com>
2023-07-15 17:51:30 +0530

    Update main.jsx
    testing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+<h2>testing</h2>
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
